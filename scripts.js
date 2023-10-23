f8f1995324e669dd96bb902ce57bba307255ae90
Gustavo Siede <124945658+avarento@users.noreply.github.com>
2023-10-23 02:28:28 -0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -8,29 +8,7 @@ file.onchange = function() {
 }
 
 btn.onclick = function() {
-    
-    const { JSDOM } = require('jsdom');
-    const data = file;
-    const dom = new JSDOM(data);
-    const { document } = dom.window;
-    let array = [];
-
-
-    for (let i = 0; document.querySelectorAll("#EdFornecedor")[i].textContent != '\nSCHERER S/A COMERCIO DE AUTOPECAS\n'; i++) {
-
-        let obj = {
-            "fornecedor": document.querySelectorAll("#EdFornecedor")[i].textContent.replace(/\n/g, ''),
-            "nota": document.querySelectorAll("#EdNota")[i].textContent.replace(/\n/g, ''),
-            "data": document.querySelectorAll("#EdData")[i].textContent.replace(/\n/g, ''),
-            "volumes": document.querySelectorAll("#edVolumes")[i].textContent.replace(/\n/g, ''),
-            "valor": document.querySelectorAll("#EdValor")[i].textContent.replace(/\n/g, '')
-        }
-        array.push(obj)
-    }
-
-
-    console.log("\nRelátório: \n ", array)
-
+    alert("Ainda sem solução...")
 }
 
 
