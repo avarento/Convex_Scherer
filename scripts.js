75957b0e21520238110e37c21ad5dbd885b6e6c3
Gustavo Siede <124945658+avarento@users.noreply.github.com>
2024-02-13 12:45:50 -0300

    Update scripts.js
    Adicionado nome de arquivo segundo data do relátório.

--- scripts.js
+++ scripts.js
@@ -3,12 +3,14 @@ const btn = document.getElementById("btn");
 const file = document.getElementById("fileUploaded");
 let html;
 let elementDOM;
+let namefile;
 
 
 file.onchange = function() {
     btn.style.display = "inline";
     upload.style.display = "none";
     const data = file.files[0];
+    let fileName = data.file
     let reader = new FileReader();  
     reader.onload = function (e) {
         html = e.target.result;
@@ -21,6 +23,7 @@ btn.onclick = function() {
     function creatDOM(htmlString) {
         let parse = new DOMParser();
         let DOM = parse.parseFromString(htmlString, 'text/html');
+        namefile = DOM.querySelector("#EdPeriodo").innerText.slice(9,19);
         let array = [];
         for (let i = 0; DOM.querySelectorAll("#EdFornecedor")[i].textContent != '\nSCHERER S/A COMERCIO DE AUTOPECAS\n'; i++) {
             let obj = {
@@ -42,7 +45,7 @@ btn.onclick = function() {
     let sheet = XLSX.utils.json_to_sheet(elementDOM)
     const workBook = XLSX.utils.book_new();
     XLSX.utils.book_append_sheet(workBook, sheet, 'Sheet 1');
-    XLSX.writeFile(workBook, './sample.xlsx');
+    XLSX.writeFile(workBook, './' + namefile + '.xlsx');
     
 }
 
