977159848e9a89b84aad7f9ac603895e2d73d9af
Gustavo Siede <124945658+avarento@users.noreply.github.com>
2024-02-13 16:18:42 -0300

    Update scripts.js
    Diferença entre datas (atualizado)

--- scripts.js
+++ scripts.js
@@ -5,6 +5,18 @@ let html;
 let elementDOM;
 let namefile;
 
+function dateDifference(a, b) {
+    let x = a.split("/");
+    const date1 = new Date(x[2] + '/' + x[1] + '/' + x[0]);
+    let y = b.split("/");
+    const date2 = new Date(y[2] + '/' + y[1] + '/' + y[0]);
+    const diffInMilliseconds = Math.abs(date2 - date1);
+    const diffInSeconds = diffInMilliseconds / 1000;
+    const diffInMinutes = diffInSeconds / 60;
+    const diffInHours = diffInMinutes / 60;
+    const diffInDays = diffInHours / 24;
+    return diffInDays
+}
 
 file.onchange = function() {
     if (file.files[0].type !== 'text/html') {
@@ -30,10 +42,10 @@ btn.onclick = function() {
         namefile = DOM.querySelector("#EdPeriodo").innerText.slice(10,20);
         let array = [];
         for (let i = 0; DOM.querySelectorAll("#EdFornecedor")[i].textContent != '\nSCHERER S/A COMERCIO DE AUTOPECAS\n'; i++) {
-            let dataEmissao = new Date (DOM.querySelectorAll("#EdData")[i].textContent.replace(/\n/g, ''));
-            let dataEntrada = new Date (DOM.querySelector("#EdPeriodo").innerText.slice(10,20));
-            let diferencaMill = dataEmissao - dataEntrada;
-            let diferencaDias = diferencaMill / (1000 * 60 * 60 * 24);
+            let dataA = DOM.querySelectorAll("#EdData")[i].textContent.replace(/\n/g, '');
+            let dataB = DOM.querySelector("#EdPeriodo").innerText.slice(10,20);
+            let diff = dateFifference(dataA, dataB);
+            
             let obj = {
                 //esqueci de usar o .innerText
                 "fornecedor": DOM.querySelectorAll("#EdFornecedor")[i].textContent.replace(/\n/g, ''),
@@ -42,7 +54,7 @@ btn.onclick = function() {
                 "datae": DOM.querySelector("#EdPeriodo").innerText.slice(10,20),
                 "volumes": DOM.querySelectorAll("#edVolumes")[i].textContent.replace(/\n/g, ''),
                 "valor": DOM.querySelectorAll("#EdValor")[i].textContent.replace(/\n/g, ''),
-                "demora": Math.floor(diferencaDias)
+                "demora": diff
             }
             array.push(obj)
         }
